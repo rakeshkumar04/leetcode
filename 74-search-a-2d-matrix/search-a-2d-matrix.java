667112349297cc755a2a2685ddfb22d41b4dced4
Rakesh Kumar <119156077+rakeshkumar04@users.noreply.github.com>
2026-02-26 11:24:21 +0530

    Time: 0 ms (100.00%) | Memory: 43.9 MB (62.16%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.java
+++ 74-search-a-2d-matrix/search-a-2d-matrix.java
@@ -1,55 +1,19 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-       int m = matrix.length;
-       int n = matrix[0].length;
+        int m = matrix.length;
+        int n = matrix[0].length;
 
-       if(m==1 && n==1) return (matrix[m-1][n-1] == target);
-
-       int row = -1;
-
-       int low = 0;
-       int high = m - 1;
-
-       while(low<=high){
-            int mid = (low + high) / 2;
-
-            if(matrix[mid][0] == target){
-                return true;
-            }
-
-            if(matrix[mid][0] > target){
-                high = mid - 1;
-            }
-
-            if(matrix[mid][0] < target){
-                row = mid;
-                low = mid + 1;
-            }
-       }
-
-       if(row == -1) return false;
-
-        return searchMatrixRow(matrix[row], target, n);
-    }
-
-    public boolean searchMatrixRow(int[] row, int target, int n){
         int low = 0;
-        int high = n - 1;
+        int high = (m * n) - 1;
 
         while(low<=high){
             int mid = (low + high) / 2;
 
-            if(row[mid] == target){
-                return true;
-            }
+            if(matrix[mid/n][mid%n] == target) return true;
 
-            if(row[mid] > target){
-                high = mid - 1;
-            }
+            if(matrix[mid/n][mid%n] > target) high = mid - 1;
 
-            if(row[mid] < target){
-                low = mid + 1;
-            }
+            if(matrix[mid/n][mid%n] < target) low = mid + 1;
         }
 
         return false;
