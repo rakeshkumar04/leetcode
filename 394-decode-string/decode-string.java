40155b4e5ecd92e6f188b928caa67e5de613ae5a
Rakesh Kumar <119156077+rakeshkumar04@users.noreply.github.com>
2026-02-26 19:42:21 +0530

    Time: 3 ms (21.25%) | Memory: 42.7 MB (86.13%) - LeetSync

--- 394-decode-string/decode-string.java
+++ 394-decode-string/decode-string.java
@@ -1,46 +1,42 @@
 class Solution {
     public String decodeString(String s) {
-       Stack<Character> st = new Stack<>();
-
-       for(char c: s.toCharArray()){
-            if(c!=']') st.push(c);
-
-            else{
-                StringBuilder temp = new StringBuilder();
-                while(!st.isEmpty() && st.peek()!='['){
-                    temp.append(st.pop());
+       // Stack to store the repeat counts for nested patterns
+        Deque<Integer> countStack = new ArrayDeque<>();
+        // Stack to store the accumulated strings before entering a bracket
+        Deque<String> stringStack = new ArrayDeque<>();
+      
+        int currentCount = 0;
+        String currentString = "";
+      
+        // Process each character in the input string
+        for (char ch : s.toCharArray()) {
+            if (Character.isDigit(ch)) {
+                // Build the number (handle multi-digit numbers)
+                currentCount = currentCount * 10 + (ch - '0');
+            } else if (ch == '[') {
+                // Opening bracket: push current state to stacks and reset
+                countStack.push(currentCount);
+                stringStack.push(currentString);
+                currentCount = 0;
+                currentString = "";
+            } else if (ch == ']') {
+                // Closing bracket: pop from stacks and construct repeated string
+                int repeatTimes = countStack.pop();
+                StringBuilder repeatedString = new StringBuilder();
+              
+                // Repeat the current string based on the count
+                for (int i = 0; i < repeatTimes; i++) {
+                    repeatedString.append(currentString);
                 }
-                temp.reverse();
-                st.pop();
-
-                StringBuilder num = new StringBuilder();
-                while(!st.isEmpty() && Character.isDigit(st.peek())){
-                    num.insert(0, st.pop());
-                }
-
-                int number = Integer.parseInt(num.toString());
-                StringBuilder repeat = new StringBuilder();
-
-                for(int i=0; i<number; i++){
-                    repeat.append(temp);
-                }
-
-                for(char ch: repeat.toString().toCharArray()){
-                    st.push(ch);
-                }
+              
+                // Combine with the previous string from the stack
+                currentString = stringStack.pop() + repeatedString.toString();
+            } else {
+                // Regular character: append to current string
+                currentString += ch;
             }
-
-            
-       }
-
-        StringBuilder res = new StringBuilder();
-            
-        while(!st.isEmpty()){
-        res.append(st.pop());
         }
-
-        res.reverse();
-
-        return res.toString();
+      
+        return currentString;
     }
 }
